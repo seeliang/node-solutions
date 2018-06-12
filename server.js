ef026dc146aeae46254a7434d807f49b060d4fc4
sihui <me@sihuiliang.com>
2018-06-12 13:42:35 +1000

    Setup server

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hi: String
+  }
+`);
+
+
+var root = {
+  hi: () => {
+    return 'Just to say hi';
+  },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
