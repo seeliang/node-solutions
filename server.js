06ab6c1977951f5922336897bfea88ae935b4f52
seeliang <me@sihuiliang.com>
2018-07-27 08:31:51 +1000

    add shall join flag in game resolver

--- server.js
+++ server.js
@@ -49,13 +49,12 @@ const joinPublisher = ({game, Publishers, publishersResolver}) => {
   return Object.assign(game, {publisher: publisher[0]});
 }
 
-const gamesResolver =({Games, Publishers, publishersResolver, joinPublisher}) => (args) => {
+const gamesResolver =({Games, Publishers, publishersResolver, joinPublisher, shallJoin}) => (args) => {
   if (args.id) {
-    const game = Games.find(game => { 
-      if (game.id === args.id) {
-        return joinPublisher({game, Publishers, publishersResolver})
-      }
-    })
+    let game = Games.find(game => game.id === args.id);
+    if (shallJoin !== false) {
+      game = joinPublisher({game, Publishers, publishersResolver});
+    }
     if (typeof game !== 'object') {
       return new Response ("NotFound",'could not find game with this id');
     }
