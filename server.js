a942db61a0442d249153a65436999e6a36afd362
seeliang <me@sihuiliang.com>
2018-07-31 22:02:13 +1000

    static path

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = require('express');
-const web = express();
+const express = require('express'),
+  path = require('path'),
+  pathTo = {
+    static: path.join(__dirname, 'assets')
+  },
+  web = express();
 
 web.get('/',(req,res)=> res.send('home'))
 web.get('/hi',(req,res)=> res.send('hi'))
+console.log(pathTo)
+web.use('/static', express.static(pathTo.static))
+
 web.listen(8000,() => console.log('start at port localhost:8000'))
