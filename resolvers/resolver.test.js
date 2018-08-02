6a3aea466d2b6434771bc6a590b6ece58bbecc24
seeliang <me@sihuiliang.com>
2018-08-02 20:27:35 +1000

    test: add join publisher

--- resolvers/resolver.test.js
+++ resolvers/resolver.test.js
@@ -2,13 +2,28 @@ const {gamesResolver,publishersResolver,joinGame,joinPublisher} = require('./ind
 
 const {expect} = require('chai');
 
+const games = [{ id: 1, title: 'MGS', publisherId: 3}],
+  publishers = [{id: 3, title: 'konami'}];
+
 describe('joinResolvers', () => {
+
   describe('joinGames', () => {
     it('should join games to publisher', () => {
-      const games = [{ id: 1, title: 'MGS', publisherId: 3}],
-        publishers = [{id: 3, title: 'konami'}];
       const result = joinGame({publishers, Games: games, gamesResolver});
       expect(result).to.deep.equal([{id:3, title: 'konami', games:[{id: 1, title:'MGS', publisherId:3}]}])
+    });
+  });
+
+  describe('joinPublisher', () => {
+    it('should join publisher to games', () => {
+      const result = joinPublisher({games, Publishers: publishers, publishersResolver});
+      expect(result).to.deep.equal(
+        [
+          {
+            id: 1, title:'MGS', publisherId:3, publisher:[{id:3, title: 'konami'}]
+          }
+        ]
+      )
     });
   });
 });
