a70b0d08c6eef0e025eceba2544d394c78db583c
seeliang <me@sihuiliang.com>
2018-08-07 08:02:55 +1000

    test: add publishers resolver

--- resolvers/resolver.test.js
+++ resolvers/resolver.test.js
@@ -3,30 +3,108 @@ const {
   gamesResolver, publishersResolver, joinGame, joinPublisher,
 } = require('./index');
 
+const oneGame = [{ id: '1', title: 'MGS', publisherId: '3' }];
 
-const games = [{ id: '1', title: 'MGS', publisherId: '3' }];
+const onePublisher = [{ id: '3', title: 'konami' }];
 
+const Games = [
+  { id: '1', title: 'metal gear solid', publisherId: '1' },
+  { id: '2', title: 'god of war', publisherId: '2' },
+  { id: '3', title: 'winning eleven', publisherId: '1' },
+];
 
-const publishers = [{ id: '3', title: 'konami' }];
+const Publishers = [
+  { id: '1', title: 'konami' },
+  { id: '2', title: 'santa monica' },
+];
+
 
 describe('the join resolvers', () => {
   describe('joinGames', () => {
     it('should join games to publisher', () => {
-      const result = joinGame({ publishers, Games: games, gamesResolver });
-      expect(result).to.deep.equal([{ id: '3', title: 'konami', games: [{ id: '1', title: 'MGS', publisherId: '3' }] }]);
+      const result = joinGame({
+        publishers: onePublisher,
+        Games: oneGame,
+        gamesResolver,
+      });
+      expect(result).to.deep.equal([{
+        id: '3',
+        title: 'konami',
+        games: [{
+          id: '1',
+          title: 'MGS',
+          publisherId: '3',
+        }],
+      }]);
     });
   });
 
   describe('joinPublisher', () => {
     it('should join publisher to games', () => {
-      const result = joinPublisher({ games, Publishers: publishers, publishersResolver });
+      const result = joinPublisher({
+        games: oneGame,
+        Publishers: onePublisher,
+        publishersResolver,
+      });
       expect(result).to.deep.equal(
         [
           {
-            id: '1', title: 'MGS', publisherId: '3', publisher: [{ id: '3', title: 'konami' }],
+            id: '1',
+            title: 'MGS',
+            publisherId: '3',
+            publisher: [{ id: '3', title: 'konami' }],
           },
         ],
       );
+    });
+  });
+
+  describe('publisherResolver', () => {
+    it('by default it should join games to publisher', () => {
+      const result = publishersResolver({
+        Publishers,
+        Games,
+        gamesResolver,
+        joinGame,
+      })({});
+
+      expect(result).to.deep.equal([{
+        id: '1',
+        title: 'konami',
+        games: [
+          { id: '1', title: 'metal gear solid', publisherId: '1' },
+          { id: '3', title: 'winning eleven', publisherId: '1' }],
+      },
+      {
+        id: '2',
+        title: 'santa monica',
+        games: [{ id: '2', title: 'god of war', publisherId: '2' }],
+      }]);
+    });
+
+    it('with correct id, it should join games to publisher', () => {
+      const result = publishersResolver({
+        Publishers,
+        Games,
+        gamesResolver,
+        joinGame,
+      })({ id: '1' });
+      expect(result).to.deep.equal([{
+        id: '1',
+        title: 'konami',
+        games: [{ id: '1', title: 'metal gear solid', publisherId: '1' },
+          { id: '3', title: 'winning eleven', publisherId: '1' }],
+      }]);
+    });
+
+    it.skip('with wrong id, it should notify', () => {
+      const result = publishersResolver({
+        Publishers,
+        Games,
+        gamesResolver,
+        joinGame,
+      })({ id: '4' });
+      // chai https://stackoverflow.com/questions/14966821/testing-for-errors-thrown-in-mocha
     });
   });
 });
