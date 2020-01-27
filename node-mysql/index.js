7641f46559857739625030953387d9d3f9f37b97
seeliang <me@sihuiliang.com>
2020-01-28 08:35:10 +1100

    feat: node mysql, initial data load

---
+++ node-mysql/index.js
@@ -0,0 +1,17 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  port: '8889',
+  database: 'demo'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * FROM `DEPARTMENT`', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results);
+});
+ 
+connection.end();
