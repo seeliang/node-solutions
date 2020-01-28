dceb352285f84469a9499afb11735ddc6e7abf5b
seeliang <me@sihuiliang.com>
2020-01-28 22:21:22 +1100

    feat: api department

--- node-mysql-api/index.js
+++ node-mysql-api/index.js
@@ -1,17 +1,25 @@
-var mysql      = require('mysql');
+const express = require("express");
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  port: '8889',
-  database: 'demo'
+  host: "localhost",
+  user: "root",
+  password: "root",
+  port: "8889",
+  database: "demo"
 });
- 
-connection.connect();
- 
-connection.query('SELECT * FROM `DEPARTMENT`', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The DEPARTMENT List`: ', results);
+
+const api = express();
+
+api.listen(3456, () => {
+  console.log("Server running on port 3456");
 });
- 
-connection.end();
+
+api.get("/get/department", async (req, res, next) => {
+  connection.connect();
+  connection.query("SELECT * FROM `DEPARTMENT`", (error, results) => {
+    if (error) throw error;
+    let departmentData = Object.values(JSON.parse(JSON.stringify(results)));
+    res.json(departmentData);
+  });
+  connection.end();
+});
