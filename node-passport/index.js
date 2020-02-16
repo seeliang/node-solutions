6f1ce8561334132b9409a1ce3bc7a5b3353b3072
seeliang <me@sihuiliang.com>
2020-02-17 08:49:06 +1100

    feat: init login

--- node-passport/index.js
+++ node-passport/index.js
@@ -2,10 +2,36 @@ const express = require('express');
 
 const server = express();
 
+// passport
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+const credentials = require('./credentials');
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+  clientID: credentials.client_id,
+  clientSecret: credentials.client_secret,
+  callbackURL: 'http://localhost:1200/profile',
+},
+((accessToken, refreshToken, profile) => {
+  console.log(profile);
+})));
+
+
+// server set
+
 server.set('view engine', 'ejs');
 
 server.get('/', (req, res) => res.render('main', { title: 'Home', message: 'Hello! homepage for you' }));
 server.get('/login', (req, res) => res.render('main', { title: 'login', message: 'please login guys!' }));
 server.get('/profile', (req, res) => res.render('main', { title: 'profile', message: 'oh it is you!' }));
+
+// auth
+server.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
 
 server.listen(1200, () => console.log('start at port localhost:1200'));
