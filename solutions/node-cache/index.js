afa2a2388f43fdd9014233b02cdc98c37f31fedb
See way Liang <sihui.liang@adevinta.com>
2022-10-14 10:43:31 +1100

    feat: add node cache

--- solutions/node-cache/index.js
+++ solutions/node-cache/index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const path = require('path');
+const NodeCache = require("node-cache");
 const web = express();
 const axios = require('axios')
 
-web.get('/', (req, res) => {
-    axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/hello', {})
-.then(function(response) {
+const cache = new NodeCache({ stdTTL: 15 });
+
+const fixedApi ='https://api.dictionaryapi.dev/api/v2/entries/en/hello'
+
+const verifyCache = (req, res, next) => {
+    try {
+      const id  = fixedApi;
+      if (cache.has(id)) {
+        return res.status(200).json(cache.get(id));
+      }
+      return next();
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+
+web.get('/', verifyCache, (req, res) => {
+    axios.get(fixedApi, {})
+.then((response) => {
+    console.log('call ext')
+    cache.set(fixedApi, response.data);
     res.json(response.data);
  
     })
