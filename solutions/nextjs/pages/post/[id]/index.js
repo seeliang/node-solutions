5fbb562594b8ede1cee5e435e8d4137e4f5f7da6
See way Liang <sihui.liang@adevinta.com>
2022-10-28 11:20:35 +1100

    chore: add dynamic content

--- solutions/nextjs/pages/post/[id]/index.js
+++ solutions/nextjs/pages/post/[id]/index.js
@@ -1,11 +1,23 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Post = () => {
+  const [content, setContent] = useState('');
   const router = useRouter();
   const { id } = router.query;
-  console.log(id);
-  return <h1>You have reached Post: {id} </h1>;
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    console.log(id);
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${id}`)
+      .then((r) => r.json())
+      .then((data) => {
+        setContent(JSON.stringify(data));
+      });
+  }, [id]);
+
+  return <><h1>You have post word: {id} </h1> <p>{content}</p></>;
 };
 
 export default Post;
