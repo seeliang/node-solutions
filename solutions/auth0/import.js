c426a285976e92d11ac7950a6c754fd958be8a62
See way Liang <hi@sihuiliang.com>
2023-02-17 15:06:44 +1100

    chore: update script https://community.auth0.com/t/bulk-user-imports-node-js-example-does-not-work/62482

--- solutions/auth0/import.js
+++ solutions/auth0/import.js
@@ -1,21 +1,25 @@
 const axios = require('axios').default;
+const FormData = require('form-data');
+const fs = require('fs');
 const cred = require('./credentials');
 
 const {
-  domain, token, file, connectionId, externalId,
+  domain, token, file, connectionId,
 } = cred;
-const options = {
-  method: 'POST',
-  url: `https://${domain}/api/v2/jobs/users-imports`,
-  headers: {
-    authorization: `Bearer ${token}`,
-    'content-type': 'multipart/form-data; boundary=---011000010111000001101001',
-  },
-  data: `-----011000010111000001101001\r\nContent-Disposition: form-data; name="users"; filename="${file}"\r\nContent-Type: text/json\r\n\r\n\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name="connection_id"\r\n\r\n${connectionId}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name="external_id"\r\n\r\n${externalId}\r\n-----011000010111000001101001--\r\n`,
+
+const script = async () => {
+  const formData = new FormData();
+  formData.append('connection_id', connectionId); // Replace with correct connection ID
+  formData.append('users', fs.createReadStream(file));
+
+  try {
+    const res = await axios.post(`https://${domain}/api/v2/jobs/users-imports`, formData, {
+      headers: { ...formData.getHeaders(), authorization: `Bearer ${token}` },
+    });
+    console.log(res.data);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-axios.request(options).then((response) => {
-  console.log(response.data);
-}).catch((error) => {
-  console.error(error);
-});
+script();
